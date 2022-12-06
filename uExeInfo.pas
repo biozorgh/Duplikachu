cb10baa09afb92fa547e0cd5375bff7489c10b51
Matthieu <83407710+biozorgh@users.noreply.github.com>
2022-12-06 13:28:47 +0100

    Ajout des bibliothèques

---
+++ uExeInfo.pas
@@ -0,0 +1,97 @@
+{*******************************************************************************
+ baLibom, Copyright (c) 2011, SMD Systems SAS
+ Matthieu LAURENT
+ unité
+*******************************************************************************}
+unit uExeInfo;
+
+interface
+
+uses Windows, sysutils;
+
+type
+ TExeInfo = packed record
+   Filled           : Boolean;
+   FileName         : string;
+   CompanyName      : string;
+   FileDescription  : string;
+   FileVersion      : string;
+   InternalName     : string;
+   LegalCopyright   : string;
+   OriginalFilename : string;
+   ProductName      : string;
+   ProductVersion   : string;
+  end;
+
+
+
+var ExeInfo : TExeInfo;
+
+
+function GetVersionInfo(FileName, InfoName : string) : string;
+function GetExeInfo(aFileName : string; var aInfo : TExeInfo) : boolean;
+
+
+implementation
+
+////////////////////////////////////////////////////////////////////////////////
+function GetVersionInfo(FileName, InfoName : string) : string;
+const EngBrit = '040904E4'; // LocaleId
+var BufSize, Len : cardinal;
+    Buf, Info : PChar;
+    SubBlock : string;
+    DataLen  : UInt;
+    LangPtr  : Pointer;
+begin
+  Result := '';
+  BufSize := 0;
+  BufSize := GetFileVersionInfoSize(PChar(FileName), BufSize);
+  if BufSize > 0 then
+  begin
+    Buf := AllocMem(BufSize);
+    GetFileVersionInfo(PChar(FileName), 0, BufSize, Buf);
+
+    if VerQueryValue(Buf,'\VarFileInfo\Translation',LangPtr, DataLen) then
+      SubBlock := Format('\StringFileInfo\%0.4x%0.4x\%s'#0,[LoWord(LongInt(LangPtr^)), HiWord(LongInt(LangPtr^)), InfoName]);
+
+    if VerQueryValue(Buf, PChar(SubBlock), Pointer(Info), Len) then
+    begin
+      if Length(Info) > 0 then
+      Result := Info;
+    end;
+
+    FreeMem(Buf, BufSize);
+  end;
+end;
+
+////////////////////////////////////////////////////////////////////////////////
+function GetExeInfo(aFileName : string; var aInfo : TExeInfo) : boolean;
+begin
+  Result         := False;
+  aInfo.Filled   := false;
+  aInfo.FileName := '';
+
+  if not FileExists(aFileName) then exit;
+
+  aInfo.Filled           := True;
+  aInfo.FileName         := aFileName;
+  aInfo.CompanyName      := GetVersionInfo(aFileName, 'CompanyName');
+  aInfo.FileDescription  := GetVersionInfo(aFileName, 'FileDescription');
+  aInfo.FileVersion      := GetVersionInfo(aFileName, 'FileVersion');
+  aInfo.InternalName     := GetVersionInfo(aFileName, 'InternalName');
+  aInfo.LegalCopyright   := GetVersionInfo(aFileName, 'LegalCopyright');
+  aInfo.OriginalFilename := GetVersionInfo(aFileName, 'OriginalFilename');
+  aInfo.ProductName      := GetVersionInfo(aFileName, 'ProductName');
+  aInfo.ProductVersion   := GetVersionInfo(aFileName, 'ProductVersion');
+  Result := True;
+end;
+
+initialization
+
+ // récupérer les informations du programe lui-même :
+  GetExeInfo(ParamStr(0), ExeInfo);
+
+finalization
+
+
+end.
---
+++ uSelectFolder.pas
@@ -0,0 +1,77 @@
+{*******************************************************************************
+ baLibom, Copyright (c) 2011, SMD Systems SAS
+ Matthieu LAURENT
+ unité
+*******************************************************************************}
+unit uSelectFolder;
+
+interface
+
+uses
+  Windows, ShellApi, ShlObj, Activex;
+
+function SelectFolder(const Caption: string; const Root: WideString; var Directory: string; Owner: HWND; CreateFolderOption : boolean): Boolean;
+
+implementation
+
+////////////////////////////////////////////////////////////////////////////////
+function SelectDirCB(Wnd: HWND; uMsg: UINT; lParam, lpData: LPARAM): Integer
+stdcall;
+begin
+  if (uMsg = BFFM_INITIALIZED) and (lpData <> 0) then
+    SendMessage(Wnd, BFFM_SETSELECTION, Integer(True), lpdata);
+  Result := 0;
+end;
+
+////////////////////////////////////////////////////////////////////////////////
+function SelectFolder(const Caption: string; const Root: WideString; var Directory: string; Owner: HWND; CreateFolderOption : boolean): Boolean;
+var
+  BrowseInfo: TBrowseInfo;
+  Buffer: PChar;
+  RootItemIDList, ItemIDList: PItemIDList;
+  ShellMalloc: IMalloc;
+  IDesktopFolder: IShellFolder;
+  Eaten, Flags: LongWord;
+begin
+  Result := False;
+  FillChar(BrowseInfo, SizeOf(BrowseInfo), 0);
+  if (ShGetMalloc(ShellMalloc) = S_OK) and (ShellMalloc <> nil) then
+  begin
+    Buffer := ShellMalloc.Alloc(MAX_PATH);
+    try
+      SHGetDesktopFolder(IDesktopFolder);
+      if Root = '' then
+        RootItemIDList := nil
+      else
+        IDesktopFolder.ParseDisplayName(Owner, nil, POleStr(Root), Eaten, RootItemIDList, Flags);
+      with BrowseInfo do
+      begin
+        hwndOwner := Owner;
+        pidlRoot := RootItemIDList;
+        pszDisplayName := Buffer;
+        lpszTitle := PChar(Caption);
+        ulFlags := BIF_RETURNONLYFSDIRS + BIF_NEWDIALOGSTYLE + BIF_DONTGOBELOWDOMAIN + BIF_STATUSTEXT;
+        {
+        if CreateFolderOption then
+         //ulFlags := ulFlags + BIF_NEWDIALOGSTYLE + BIF_EDITBOX;
+         ulFlags := ulFlags + BIF_NEWDIALOGSTYLE + $0100; //BIF_UAHINT;
+         // http://msdn.microsoft.com/en-us/library/windows/desktop/bb773205%28v=vs.85%29.aspx
+        } 
+        lpfn := SelectDirCB;
+        lparam := Integer(PChar(Directory));
+      end;
+      ItemIDList := ShBrowseForFolder(BrowseInfo);
+      Result :=  ItemIDList <> nil;
+      if Result then
+      begin
+        ShGetPathFromIDList(ItemIDList, Buffer);
+        ShellMalloc.Free(ItemIDList);
+        Directory := Buffer;
+      end;
+    finally
+      ShellMalloc.Free(Buffer);
+    end;
+  end;
+end;
+
+end.
